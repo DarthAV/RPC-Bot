5ead6858e8e5783d0ba8b54e55ffcccd4bb51983
DarthAV <Ansh.Verma22@outlook.com>
2021-06-07 08:46:48 -0700

    fix bug in template

--- src/rps/BotTemplate.java
+++ src/rps/BotTemplate.java
@@ -5,19 +5,19 @@
 package rps;
 
 public class BotTemplate {
-	/**
-	 * Returns one char representing rock, paper, or scissors
-	 * @return 'r', 'p', or 's'
-	 */
-	public char play() {
-		return 'r';
-	}
-	
-	/**
-	 * Gets the result of a match with another bot, so you can adapt.
-	 */
-	public void getResult(char yourMove, char theirMove) {
-		
-	}
-}
+    /**
+     * Returns one char representing rock, paper, or scissors
+     * 
+     * @return 'r', 'p', or 's'
+     */
+    public char play() {
+        return 'r';
+    }
+
+    /**
+     * Gets the result of a match with another bot, so you can adapt.
+     */
+    public void getResult(char yourMove, char theirMove) {
+
+    }
 }
